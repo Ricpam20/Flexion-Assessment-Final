8b4cade698c09a18f790a8cecbd6b0b9d2404818
Ricpam20 <matt.prat19@gmail.com>
2023-07-20 01:13:38 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,6 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
-# Check and install Maven 3
-if ! command_exists mvn; then
-  echo "Maven not found. Installing Maven 3..."
-  sudo yum update
-  sudo yum install -y maven
-else
-  echo "Maven is already installed."
-fi
-
 # Check and install Java 11
 if ! command_exists java; then
   echo "Java 11 not found. Installing OpenJDK 11..."
@@ -23,18 +14,9 @@ else
   echo "Java 11 is already installed."
 fi
 
-# Check and install Node.js 18 (only if Node.js 18 is available)
-if ! command_exists node; then
-  echo "Node.js not found. Installing Node.js 18..."
 
-  # Add NodeSource repository for Node.js 18
-  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-
-  # Install Node.js
-  sudo yum install -y nodejs
-else
-  echo "Node.js is already installed."
-fi
+# Check and install Httpd 
+sudo yum install httpd
 
 # Show installed versions
 echo "Installed Versions:"
@@ -42,16 +24,11 @@ mvn -version
 java -version
 node -v
 npm -v
-ls
-pwd
 
+#kill running processes
 sudo fuser -k 3000/tcp
 sudo fuser -k 8080/tcp
 
 #boot applications
 cd ~/backend/
 nohup java -jar assessment 0.0.1-SNAPSHOT.jar 
-
-cd ~/frontend/
-npm install
-npm start
