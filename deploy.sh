16c892ea653248518d3a22549094ede25331d50f
Ricpam20 <matt.prat19@gmail.com>
2023-07-20 06:00:29 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,12 @@ command_exists() {
 # Check and install Java 11
 if ! command_exists java; then
   echo "Java 11 not found. Installing OpenJDK 11..."
-  sudo yum update
-  sudo yum install -y java-11-openjdk
+  sudo curl -O https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
+  sudo tar zxvf openjdk-11.0.1_linux-x64_bin.tar.gz
+  sudo mv jdk-11.0.1 /usr/local/
+
+  sudo echo "export JAVA_HOME=/usr/local/jdk-11.0.1\n export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile.d/jdk11.sh
+  sudo source /etc/profile.d/jdk11.sh
 else
   echo "Java 11 is already installed."
 fi
@@ -26,8 +30,8 @@ sudo fuser -k 3000/tcp
 sudo fuser -k 8080/tcp
 
 #boot applications
+sudo cp -r frontend/* /var/www/html/
 cd ~/backend/
-cp ~/frontend/* /var/www/html
-nohup java -jar assessment 0.0.1-SNAPSHOT.jar 
+sudo nohup java -jar assessment 0.0.1-SNAPSHOT.jar 
 
 
